7279f354a2c4f5361afaf9c075d9f9b7581521cd
huhao <hao.hu@qq.com>
2020-01-28 23:03:56 +0800

    调整数据采用优先级

--- dataset.py
+++ dataset.py
@@ -71,7 +71,7 @@ csv_file = "Wuhan-2019-nCoV.csv"
 json_file = "Wuhan-2019-nCoV.json"
 
 df = pd.read_csv(csv_file)
-report_df_list = [pd.read_csv(os.path.join("ReportData", x)) for x in os.listdir("ReportData")]
+report_df_list = [pd.read_csv(os.path.join("ReportData", x)) for x in sorted(os.listdir("ReportData"))]
 df["date"] = df["date"].map(
     lambda x: "-".join([i.zfill(2) for i in re.split("\\D+", x)]))
 df = pd.concat([df, cn_area_df, cn_global_df, cn_day_df] + report_df_list, sort=False)
